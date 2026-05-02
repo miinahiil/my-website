41ded89311f34a169fdce7d363c73e8907482e6a
miinahiil <minahilldesigns@gmail.com>
2026-05-02 13:06:12 +0500

    Add files via upload

--- java.js
+++ java.js
@@ -1,105 +1,66 @@
-// 1. Grocery Bill Calculator
+// Method 1
+// slice()
+// Cuts a part of a string using start and end index. Supports negative values.
+let word = "minahil";
+console.log(word.slice(0, 4));
+// slice()with negtiv value 
+let text = "Webappdevelopment";
+console.log(text.slice(-6, -3));
 
-function calculateBill(items) {
-    let total = 0;
-    for (let i = 1; i <= items; i++) {
-        let price = parseFloat(prompt("Enter price of item " + i + ":"));
-        total += price;
-    }
-    return total;
-}
-let items = parseInt(prompt("Enter number of items:"));
-let total = calculateBill(items);
-if (total > 5000) {
-    total = total - (total * 0.10);
-}
-console.log("Final Bill: " + total);
+// Method 2
+// substring()
+// Cuts part of a string using index, but does NOT support negative values.
+let text1 = "Javascript";
+console.log(text1.substring(0, 4));
 
+// Method 3
+// toUpperCase()
+// Converts every letter in a string to UPPERCASE . It does not change the original string, it returns a new one.
+let name = "minahil";
+ let result = name.toUpperCase();
+ console.log(result);
 
+// Method 4
+// toLowerCase()
+// Converts all characters in a string to lowercase letters.
+let email = "MINAHIL@GMAIL.COM";
+let data = email.toLowerCase();
+console.log(data);
 
-// 2. Student Result System
+// Method 5
+// replace()
+// Replaces part of a string with another value (only first match).
+let msg = "Hello World";
+console.log(msg.replace("World", "Ahmad"));
 
-function calculateGrade(avg) {
-    if (avg >= 80) return "A";
-    else if (avg >= 60) return "B";
-    else if (avg >= 40) return "C";
-    else return "Fail";
-}
-let name = prompt("Enter student name:");
-let total = 0;
-for (let i = 1; i <= 5; i++) {
-    let marks = parseInt(prompt("Enter marks of subject " + i + ":"));
-    total += marks;
-}
-let avg = total / 5;
-let grade = calculateGrade(avg);
-console.log("Name: " + name);
-console.log("Total: " + total);
-console.log("Average: " + avg);
-console.log("Grade: " + grade);
+// Method 6
+// includes()
+// Checks if a string contains a specific word or character.
+let sentence = "I love JavaScript";
+console.log(sentence.includes("love"));
 
+// Method 7
+// indexOf()
+// Returns the position (index) of a word in a string. If not found, returns -1.
+let text2 = "Hello World";
+console.log(text2.indexOf("World"));
 
+// Method 8
+// trim()
+// Removes spaces from the beginning and end of a string.
+let username = "   Minahil   ";
+let user = username.trim();
+console.log(user);
 
-// 3. ATM Withdrawal System
+// Method 9
+// concat()
+// Joins two or more strings together.
+let first = "Minahil";
+let last = "Fatima";
+console.log(first.concat(" ", last));
 
-function withdrawMoney(balance, amount) {
-    if (amount > balance) {
-        console.log("Insufficient balance");
-        return balance;
-    } else {
-        return balance - amount;
-    }
-}
-let balance = 10000;
-let attempts = 3;
-while (attempts > 0) {
-    let amount = parseFloat(prompt("Enter amount to withdraw:"));
-    let newBalance = withdrawMoney(balance, amount);
-    if (newBalance !== balance) {
-        balance = newBalance;
-        break;
-    }
-    attempts--;
-    console.log("Attempts left: " + attempts);
-}
-console.log("Remaining Balance: " + balance);
-
-
-
-// 4. Restaurant Order System
-
-function calculateOrder(choice, qty) {
-    let price = 0;
-    if (choice == 1) price = 500;
-    else if (choice == 2) price = 1200;
-    else if (choice == 3) price = 200;
-    return price * qty;
-}
-console.log("Menu:\n1. Burger = 500\n2. Pizza = 1200\n3. Drink = 200");
-let choice = parseInt(prompt("Enter your choice:"));
-let qty = parseInt(prompt("Enter quantity:"));
-let total = calculateOrder(choice, qty);
-if (total > 2000) {
-    total -= total * 0.15;
-}
-console.log("Final Bill: " + total);
-
-
-
-// 5. Employee Salary Calculator
-
-function calculateSalary(hours) {
-    let rate = 500;
-    if (hours > 40) {
-        return (40 * rate) + ((hours - 40) * rate * 1.5);
-    } else {
-        return hours * rate;
-    }
-}
-for (let i = 1; i <= 3; i++) {
-    let name = prompt("Enter employee name:");
-    let hours = parseInt(prompt("Enter hours worked:"));
-    let salary = calculateSalary(hours);
-    console.log("Employee: " + name);
-    console.log("Salary: " + salary);
-}
+// Method 10
+// split()
+// Converts a string into an array by splitting it.
+let data1 = "red,blue,green";
+console.log(data1.split(","));
